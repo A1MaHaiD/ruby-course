70847f57040063367497c88caec3c2b2e9ac5602
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-17 20:18:17 +0300

    add linear_algoritms_task_2.drawio & algoritm_1.rb & linear_algoritms_task_2.svg & linear_algoritms_task_3.drawio

--- algoritm_1.rb
+++ algoritm_1.rb
@@ -1,14 +1,48 @@
 # task 1
-puts ("Enter number 1: ")
-number1 = gets.to_i
-puts ("Enter number 2: ")
-number2 = gets.to_i
-sum = number1 + number2
-sum_sqr = (number1 * number1) + (number2 * number2)
-sqr_sum = sum * sum
+# puts ("Enter number 1: ")
+# number1 = gets.to_i
+# puts ("Enter number 2: ")
+# number2 = gets.to_i
+# sum = number1 + number2
+# sum_sqr = (number1 * number1) + (number2 * number2)
+# sqr_sum = sum * sum
+# puts ("Sum is: " + sum.to_s)
+# puts ("Sum of square are: " + sum_sqr.to_s)
+# puts ("Square of sum are: " + sqr_sum.to_s)
 
-puts ("Sum is: " + sum.to_s)
-puts ("Sum of square are: " + sum_sqr.to_s)
-puts ("Square of sum are: " + sqr_sum.to_s)
 # task 2
+# print 'a = '
+# a = gets.to_i
+# print 'b = '
+# b = gets.to_i
+# print 'c = '
+# c = gets.to_i
+# perimeter = a + b + c
+# puts perimeter
 
+# task 3
+# print 'a = '
+# a = gets.to_i
+# print 'b = '
+# b = gets.to_i
+# hypotenuse = Math.sqrt(a**2 + b**2)
+# puts "Гіпотенуза: #{hypotenuse.to_i}"
+
+# task 4
+# puts 'Enter radius:'
+# r = gets.to_i
+# d = 2 * r
+# a = d / Math.sqrt(2)
+# puts "Length side square: #{a}"
+
+# task 5
+# puts 'Enter x1, y1, x2, y2'
+# x1 = gets.to_i
+# y1 = gets.to_i
+# x2 = gets.to_i
+# y2 = gets.to_i
+# x = (x1 + x2) / 2
+# y = (y1 + y2) / 2
+# puts "x = #{x}, y = #{y}"
+
+# task 6
