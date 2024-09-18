ac4995bdddfac3b6a620696fda698180d3093fa3
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-18 09:26:04 +0300

    add packages

--- asterisk.rb
+++
@@ -1,4 +0,0 @@
-asterisk = "*"
-new_line = "\n"
-five_asterisk = asterisk*5
-puts "The END" + new_line * 5 + five_asterisk
--- concatenation.rb
+++
@@ -1,7 +0,0 @@
-hello = "Hello"
-name = "Svitlana"
-age = 25
-wellcome_message = hello + "" + name + ".You are " + age.to_s + " years old." 
-apoligies_message = "My appoligies, becase I'm without a peresent"
-puts wellcome_message
-print apoligies_message
