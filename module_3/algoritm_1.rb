eef59f3754133cfe790fc5814bdecaa049bcbdcd
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-20 12:46:56 +0300

    add # task 9

--- module_3/algoritm_1.rb
+++ module_3/algoritm_1.rb
@@ -56,4 +56,36 @@
 # puts s
 
 # task 7
+# print "Enter cost per page = "
+# cost = gets.strip.to_i
+# print "Enter count page = "
+# count = gets.strip.to_i
+# salary = cost * count
+# puts salary
+
+# task 8
+# print "Enter cost a one number magazine per period = "
+# cost = gets.strip.to_i
+# print "Enter period of released magazine = "
+# period = gets.strip.to_i
+# total_cost = 365 / period * cost
+# puts total_cost
+
+# task 9
+# print("Enter how old your mather = ")
+# lifetime_parent1 = gets.chomp.to_i
+# print("Enter how old your father = ")
+# lifetime_parent2 = gets.chomp.to_i
+# print("Enter how old your grandmother on father tree = ")
+# lifetime_grandmother1 = gets.chomp.to_i
+# print("Enter how old your grandfather on father tree = ")
+# lifetime_grandfather1 = gets.chomp.to_i
+# print("Enter how old your grandmother on mother tree = ")
+# lifetime_grandmother2 = gets.chomp.to_i
+# print("Enter how old your grandfather on mother tree = ")
+# lifetime_grandfather2 = gets.chomp.to_i
+# lifetime = (lifetime_parent1+lifetime_parent2+lifetime_grandfather1+lifetime_grandmother1+lifetime_grandfather2+lifetime_grandmother2) / 6.to_f
+# puts ("Your lifetime is : #{lifetime}")
+
+# task 10
 
