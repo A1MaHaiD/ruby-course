3298e697d8435c8f3407470a3efb0ce03bf0bda8
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-21 10:35:58 +0300

    infalte algoritm_2.rb : execute task_1 .. task_6

---
+++ module_4/algoritm_2.rb
@@ -0,0 +1,60 @@
+# task_1
+# price = 2.30
+# print "Enter day of the week = "
+# day = gets.strip.to_i
+# print "Enter how are long time you calling = "
+# time = gets.strip.to_i
+# day == 6 || day == 7 ? price *= 0.8 : price
+# cost = price * time
+# puts(cost)
+
+# task_2
+# print "Enter number = "
+# number = gets.strip.to_i
+# if (number > 0) then rez = "Number is positive"
+#   elsif(number < 0) then rez = "Number is negative"
+#     else rez = "Number is zero"
+#     end
+# puts(rez)
+
+# task_3
+# print "Enter how old Sasha = "
+# age_sasha = gets.strip.to_i
+# print "Enter how old Tanya = "
+# age_tanya = gets.strip.to_i
+# if (age_sasha > age_tanya) then rez = "Sasha is older”."
+#   elsif(age_sasha < age_tanya) then rez = "Tanya is older"
+#     else rez = "Sasha and Tanya are the same age"
+#     end
+# puts(rez)
+
+# task_4
+# print("Enter how many years your experience = ")
+# experience = gets.strip.to_i
+# print("Enter how old you = ")
+# age = gets.strip.to_i
+# experience >= 5 && age <= 40 ? rez = "Candidate is suitable" : rez = "Candidate is not suitable"
+# puts rez
+
+# task_5
+# print "Enter a = "
+# a = gets.strip.to_i
+# print "Enter b = "
+# b = gets.strip.to_i
+# print "Enter c = "
+# c = gets.strip.to_i
+# a >= b ? m = a : m = b
+# m < c ? m = c : m
+# puts m
+
+# task_6
+# print "Enter temperature = "
+# temp = gets.strip.to_i
+# if (temp <= 0) then rez = "A cold, isn't it?"
+#   elsif(temp > 0 && temp < 10) then rez = "Cool"
+#     elsif(temp >= 10 && temp <= 20) then rez = "Nice weather we’re having"
+#       else rez = "Too hot, isn’t it?"
+#       end
+# puts rez
+
+# task_7
