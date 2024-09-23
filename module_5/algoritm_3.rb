4404d7b09003e8a7873efbeaee07837ce4362558
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-23 19:03:28 +0300

    add break_&_next_operator.rb & algoritm_3.rb

---
+++ module_5/algoritm_3.rb
@@ -0,0 +1,25 @@
+# Task 1  
+
+# print "Enter timers number: "
+# n = gets.strip.to_i
+# while n >= 1
+#   puts n
+#   n -= 1
+# end
+# puts "Start!"
+
+# Task 2
+
+# print "Enter a = "
+# a = gets.strip.to_i
+# print "Enter b = "
+# b = gets.strip.to_i
+# print "Enter c = "
+# c = gets.strip.to_i
+# while a <= b
+#   a % c == 0 ? print("#{a} ") : a
+#   a += 1
+# end
+
+# Task 3
+
