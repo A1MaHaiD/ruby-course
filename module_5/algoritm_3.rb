a6944d33d59af4e551415ee41d0a845d2cc8ebfe
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-24 20:20:16 +0300

    add algoritm.rb : homework

--- module_5/algoritm_3.rb
+++ module_5/algoritm_3.rb
@@ -23,3 +23,30 @@
 
 # Task 3
 
+
+
+# Homework 5.1
+
+# puts "Введіть ціну цукерок для 1 кг"
+# price = gets.strip.to_i
+# 5.times{|i| puts "Введіть в кг вагу цукерок, ціну яких Ви хочете дізнатися"
+#    cost = gets.strip.to_f
+#     puts (price * cost)}
+
+# Homework 5.2
+
+# divider = 21
+# count = 0
+# while count < 5
+#   print("Введіть число, яке буде ділитися на #{divider} : ")
+#   answer = gets.strip.to_i
+#   if( answer == divider)
+#     puts("Число не повинно дорівнювати #{divider}")
+#     count += 1
+#   elsif (answer % divider != 0)
+#     count += 1
+#   puts("Невірно. Спробуйте ще раз")
+# else break
+# end
+# end
+# count < 5 ? puts("Чудово") : puts("Кількість спроб завершилася :(")
