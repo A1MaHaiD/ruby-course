cb2c08847395a0e06981cfb76bf60fcc5e627173
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-09-26 08:38:01 +0300

    methods.rb : add Homework 6,1..6,2

--- module_6/methods.rb
+++ module_6/methods.rb
@@ -1,4 +1,19 @@
-def triangle_square(a, b, c)
-  p = (a + b + c) / 2
-  Math.sqrt(p * (p - a) * (p - b) * (p - c))
-end
+# def triangle_square(a, b, c)
+#   p = (a + b + c) / 2
+#   Math.sqrt(p * (p - a) * (p - b) * (p - c))
+# end
+
+# Homework 6.1
+
+# def print_welcome(name)
+#   puts("Hello, #{name}!")
+# end
+
+# Homework 6.2
+
+# def rectangle_length(a, b)
+#  if (a <= 0 && b <= 0)
+#   return nil
+# else return p = 2 * (a + b)
+# end
+# end
