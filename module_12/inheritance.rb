115ede0bbd877a5abf5921b111e4d759c5d4d4ff
A1MaHaiD <super.lukianchuk2010@gmail.com>
2024-10-27 21:45:19 +0200

    inheritance.rb : add Animal & Employee

--- module_12/inheritance.rb
+++ module_12/inheritance.rb
@@ -80,3 +80,154 @@ class ThreeFields < TwoFields
         "#{super}, #{third}"
     end
 end
+
+# Багаторівневе налідування
+
+class Animal
+    attr_accessor :name
+
+    def type
+        puts 'Animal Type'
+    end
+
+    def my_name
+        "My name is #{name}"
+    end
+end
+
+class Mammal < Animal
+    def type
+        puts 'Mammal Type'
+    end
+end
+
+class Horse < Mammal
+
+end
+
+horse = Horse.new  
+horse.type                      # Mammal Type
+puts horse.my_name              # My name is
+puts horse.my_name('other')     # Error: wrong number of argument
+
+# super та super()
+
+class Animal
+    def vararg_method(*a)
+        if(a.length == 0)
+            return 'nothing'
+        else
+            return "#{self.class.name}
+            Type with #{a} args"
+        end
+    end
+end
+
+class Mammal < Animal
+    def vararg_method(*a)
+        super()
+    end
+end
+
+class Reptile < Animal
+    def vararg_method(*a)
+        super
+    end
+end
+
+horse = Mammal.new
+puts horse.vararg_method('1')       # nothing
+
+crocodile = Reptile.new
+puts
+crocodile.vararg_method('2')        # Reptile Type ["2"] args
+
+# Модифікатори доступу
+
+class Animal
+    def public_method
+        puts 'public method'
+    end
+
+    def private_method
+        puts 'private method'
+    end
+
+    private :private_method
+end
+
+class Mammal < Animal
+  def public_method
+    super
+  end
+
+  def private_mammal_method
+    private_method
+  end
+end
+
+horse = Mammal.new
+horse.public_method                 # public method
+horse.private_mammal_method         # private method
+horse.private_method                # (NoMethodError)
+
+# Employee
+
+class Employee
+    attr_accessor :firstname, :lastname, :postition, :rate
+
+  def initialize(fn, ln, postition)
+    @firstname = fn
+    @lastname = ln
+    @postition = postition
+  end
+
+  private
+  def tax(money)
+    rule = { 10_000 => 0.1, 20_000 => 0.15, 'other': 0.3}
+    result = 0
+    rest = money
+    prev_key = 0
+    rule.each_pair  {|key, value|
+        if key == :other
+            result += rest *value
+        elsif money > key
+            result += (key - prev_key) * value
+            rest = money - key
+            prev_key = key
+        else
+            result += rest * value
+            return result
+        end    
+    }
+    end
+end
+
+class RegularEmployee < Employee
+    @@working_day = 1
+
+  def self.wd(days)
+    @@working_day = days
+  end
+
+  def month_salary(days)
+    before = rate * days/@@working_day
+    before - tax(before)
+  end
+end
+
+class HourlyEmployee < Employee
+  def month_salary(hours)
+    before = rate * hours
+    before - tax(before)
+  end
+end
+
+empl1 = RegularEmployee.new('Ivan', 'Petrenko', 'junior')
+RegularEmployee.wd(28)
+empl1.rate = 10_000
+puts empl1.month_salary(1)      # 321.3
+
+empl2 = HourlyEmployee.new('Oleksandr', 'Novak', 'manager')
+empl2.rate = 10_000
+puts empl2.month_salary(1)      # 9000.0
